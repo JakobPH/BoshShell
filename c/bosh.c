57c07d2fc58238fd427a5bb9641e892269c361bd
erem94 <erem@itu.dk>
2016-09-27 15:02:37 +0200

    Redirection worksgit status

--- c/bosh.c
+++ c/bosh.c
@@ -11,9 +11,15 @@
 #include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include "parser.h"
 #include "print.h"
 #include "forback.h"
+#include "redirect.h"
 
 /* --- symbolic constants --- */
 #define HOSTNAMEMAX 100
@@ -29,26 +35,35 @@ char *gethostname(char *hostname)
 /* --- execute a shell command --- */
 int executeshellcmd (Shellcmd *shellcmd)
 {
-  //pid = fork();
-
   printshellcmd(shellcmd);
-
   Cmd * cmdlist = shellcmd->the_cmds; 
-
+  
+  int fid, fod;
   char * in  = shellcmd->rd_stdin;
   char * out = shellcmd->rd_stdout;
   char **args = cmdlist->cmd;
   
-  Cmd *cmdlist = shellcmd->the_cmds;
   cmdlist = cmdlist->next;
 
   printf("cmd: %s\n", *args);
   printf("cmdlist: %s\n", cmdlist);
 
-  //execvp(*args, &args[0]);
-  //runcmd(shellcmd);
-  //foregroundcmd(*args, &args[0]);
+  if(out != NULL)
+  {
+    close(1);   
+    fod = open(out, O_CREAT | O_WRONLY);  
+    dup2(fod, 1);
+  }
+  if(in != NULL)
+  {
+    close(0);
+    fid = open(in, O_RDONLY);
+    dup(fid, 0);
+  }
+
   shellcmd->background ? foregroundcmd(*args, &args[0]) : backgroundcmd(*args, &args[0]);
+  close(fid);
+  close(fod);
   return 0;
 }
 
@@ -81,28 +96,4 @@ int main(int argc, char* argv[]) {
     
   return EXIT_SUCCESS;
 }
-/* 
-  printf("Shellcommand:\n");
-  printf("   stdin : %s\n", shellcmd->rd_stdin  ? shellcmd->rd_stdin  : "<none>" );
-  printf("   stdout: %s\n", shellcmd->rd_stdout ? shellcmd->rd_stdout : "<none>" );
-  printf("   bg    : %s\n", shellcmd->background ? "yes" : "no");
-  Cmd *cmdlist = shellcmd->the_cmds;
-  while (cmdlist != NULL) {
-    char **cmd = cmdlist->cmd;
-    cmdlist = cmdlist->next;
-
-    char **printcmd = cmd;
-    printf("    [");
-    while (*printcmd != NULL) {
-      printf("%s ", *printcmd++); // print the cmd and arguments
-    }
-    printf("]\n");
----------------
- Cmd *cmdlist = shellcmd->the_cmds;
-  while(cmdlist != NULL)
-  {
-    char **cmd = cmdlist->cmd;
-    cmdlist = cmdlist->next;
-
-*/
 
